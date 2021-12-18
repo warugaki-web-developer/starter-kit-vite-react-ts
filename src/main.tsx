cb627f8aefb7adb2574eed84d5e45cac66fa82a8
warugaki <warugaki.web.developer@gmail.com>
2021-12-18 10:20:23 +0900

    emotionを使ったglobalcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
+import { Global, css } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap');
+
+        html {
+          font-family: Nunito, sans-serif;
+        }
+
+        *,
+        *::after,
+        *::before {
+          box-sizing: border-box;
+        }
+      `}
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
