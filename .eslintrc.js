fae454051ebbdbc6bad2d18c85791ff72de073d1
warugaki <warugaki.web.developer@gmail.com>
2021-12-28 11:23:59 +0900

    npx sb@next automigrate

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:storybook/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
